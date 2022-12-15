95dd4e0098cc648918aaa39d93f5a906ad8f1716
Carlo Dee <carlodee.official@gmail.com>
2022-12-15 11:46:22 +0800

    Removed unnecessary inherit.

--- twrp_RMX3491.mk
+++ twrp_RMX3491.mk
@@ -6,9 +6,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
