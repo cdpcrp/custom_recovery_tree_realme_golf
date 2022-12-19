46e9925ca6e949e253913b6d764ca2712c1b93f0
Carlo Dee <carlodee.official@gmail.com>
2022-12-19 10:21:14 +0800

    Cleanup: Removed unnecessary comment spaces.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,6 @@
-#
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RMX3491.mk
--- device.mk
+++ device.mk
@@ -1,9 +1,6 @@
-#
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 LOCAL_PATH := device/realme/RMX3491
 
