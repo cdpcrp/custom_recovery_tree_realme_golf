a214092aa1f7c54c83e3b9b29f25845e22446d6a
Carlo Dee <carlodee.official@gmail.com>
2022-12-19 10:25:09 +0800

    Cleanup: Removed OMNI and PBRP support in vendor inherit.
    I will soon add PBRP in new branch though. But as for this branch, I will only make it for TWRP and OFRP.

--- twrp_RMX3491.mk
+++ twrp_RMX3491.mk
@@ -1,9 +1,6 @@
-#
 # Copyright (C) 2022 The Android Open Source Project
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -11,18 +8,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common twrp/pb/omni stuff.
 $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
-$(call inherit-product-if-exists, vendor/pb/config/common.mk)
-$(call inherit-product-if-exists, vendor/omni/config/common.mk)
 
 # Inherit from cannong device
 $(call inherit-product, device/realme/RMX3491/device.mk)
 
 PRODUCT_RELEASE_NAME := RMX3491
-PRODUCT_DEVICE := RMX3491
-PRODUCT_NAME := twrp_RMX3491
-PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 9i
-PRODUCT_MANUFACTURER := realme
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme 9i 4G
+PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
 
 PRODUCT_GMS_CLIENTID_BASE := android-oplus
 
