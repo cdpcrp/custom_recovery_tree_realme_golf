e923541fea395b088c6ae3926ad93ba53a269889
Carlo Dee <carlodee.official@gmail.com>
2022-12-24 13:11:11 +0800

    Cleanup: Change device codename and added new names in asserts.
    I found the name here: https://gist.github.com/techyminati/61325feaa61526e098a124b89ddb4d15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 # SPDX-License-Identifier: Apache-2.0
 
-DEVICE_PATH := device/realme/RMX3491
+DEVICE_PATH := device/realme/golf2
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -23,15 +23,13 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_NO_BOOTLOADER := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := RMX3491,RMX3493,RMX3491T2,ossi,qssi
+TARGET_OTA_ASSERT_DEVICE := RMX3491,RMX3493,RMX3491T2,RED8C1L1,ossi,qssi
 
 # File System
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 100663296 
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 
 # Workaround in case of copy error
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
@@ -50,13 +48,14 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
 
 # Kernel Source
-TARGET_KERNEL_CONFIG := RMX3491_defconfig
-TARGET_KERNEL_SOURCE := kernel/realme/RMX3491
+TARGET_KERNEL_CONFIG := golf2_defconfig
+TARGET_KERNEL_SOURCE := kernel/realme/golf2
 TARGET_KERNEL_CLANG_COMPILE := true
 
 # Kernel Prebuilt
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
 # Args
@@ -84,77 +83,34 @@ BOARD_SUPER_PARTITION_GROUPS := main
 BOARD_MAIN_PARTITION_LIST := system vendor product 
 BOARD_MAIN_SIZE := 9122611200 # TODO: Fix hardcoded value.
 BOARD_ROOT_EXTRA_FOLDERS := bluetooth dsp firmware persist
-
-# Platform
-TARGET_BOARD_PLATFORM := bengal # TODO: Fix hardcoded value.
+BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+
+# Some Recovery-related Properties
+TARGET_BOARD_PLATFORM := bengal
 TARGET_BOARD_PLATFORM := $(TARGET_BOARD_PLATFORM)
-QCOM_BOARD_PLATFORMS := $(TARGET_BOARD_PLATFORM)
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_USES_UEFI := true
-
-# Recovery
-BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_USES_MKE2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
-TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
-# Recovery Modules
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hidl.allocator@1.0 \
-    android.hidl.memory@1.0 \
-    android.hidl.memory.token@1.0 \
-    libdmabufheap \
-    libhidlmemory \
-    libion \
-    libnetutils \
-    libxml2
+# Qualcomm
+QCOM_BOARD_PLATFORMS := $(TARGET_BOARD_PLATFORM)
 
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.allocator@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.memory.token@1.0.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libhidlmemory.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
-
-# Hack: prevent anti rollback
+# Anti-Roll Back
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 99.87.36
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
-BOARD_USES_QCOM_FBE_DECRYPTION := true
 
 # TWRP Configuration
-TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-TW_INCLUDE_FBE_METADATA_DECRYPT := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_NTFS_3G := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
-TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 900
-TW_NO_SCREEN_BLANK := true
-TW_INCLUDE_FUSE_EXFAT := true
-TW_INCLUDE_FASTBOOTD := true
-TW_USE_FSCRYPT_POLICY := 2
-BOARD_SUPPRESS_SECURE_ERASE := true
-TARGET_USES_MKE2FS := true
-TW_DEVICE_VERSION := Crypton
-
-# Debugging Configs
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
+include $(DEVICE_PATH)/BoardConfigRecovery.mk
 
 # Others
 LC_ALL="C"
