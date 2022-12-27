929dc005527d60759183907a1023bf85510f5542
C:\Users\Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-12-28 00:12:51 +0800

    Added more override props for decryption to work.
    Attemp to fix decryption to work with lockscreen.

--- BoardConfigRecovery.mk
+++ BoardConfigRecovery.mk
@@ -47,4 +47,4 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
 # Property Override
-TW_OVERRIDE_SYSTEM_PROPS := "ro.build.fingerprint;ro.build.version.incremental;ro.product.system.device"
+TW_OVERRIDE_SYSTEM_PROPS := "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
