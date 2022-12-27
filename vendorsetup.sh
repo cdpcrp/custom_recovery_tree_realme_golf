95091b957cfeaeb6eee9899d02766fceb3b9da1c
C:\Users\Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-12-27 19:27:37 +0800

    Added system.device name; bengal

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -56,7 +56,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_USE_GREEN_LED=0
 
 	# Device Codename (other variant)
-	export TARGET_DEVICE_ALT="RMX3491,RMX3491T2,RMX3493,RED8C1L1,ossi,qssi"
+	export TARGET_DEVICE_ALT="RMX3491,RMX3491T2,RMX3493,RED8C1L1,ossi,qssi,bengal"
 
 	# Partitions Handling
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
