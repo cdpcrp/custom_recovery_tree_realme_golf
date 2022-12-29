36e1fd72686cf996296fa5442d2bf42da14207ef
Oracle-GSI <121375771+Oracle-GSI@users.noreply.github.com>
2022-12-30 00:55:56 +0300

    Update twrp_golf2.mk

--- twrp_golf2.mk
+++ twrp_golf2.mk
@@ -5,6 +5,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from Custom Recovery vendor-common.
 $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
