af0ff9f73008f031bd7f95aae23505a388fbb914
Carlo Dee <carlodee.official@gmail.com>
2023-01-17 21:27:13 +0800

    Update BoardConfig.mk
    Signed-off-by: Carlo Dee <carlodee.official@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,8 +96,8 @@ TARGET_USES_MKE2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP := $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/properties/system.prop
+TARGET_VENDOR_PROP := $(DEVICE_PATH)/properties/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 
